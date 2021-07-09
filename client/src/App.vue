a189586680529b36e91a0bb374e348a3d31f8d5f
Meron Abate <meriab1234@gmail.com>
2021-07-09 15:55:30 +0300

    clean again

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,48 +1,8 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-title>
-          WCSCMS
-        </v-list-title>
-        <v-list-item-subtitle>
-          MEVN full stack App
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-      <v-divider></v-divider>
-       <v-list dense>
-      <v-list-item-group
-        color="primary"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i" :to="item.link"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app color="blue-grey darken-3" dark >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
+  <v-app>
+   <v-content>
      <router-view></router-view>
-    </v-main>
+   </v-content>
     <v-footer
     color="blue-grey darken-3" dark
     
@@ -74,13 +34,13 @@
 
 <script>
   export default {
-    data: () => ({ drawer: null,
-     items: [
-        { title: 'Home', icon: 'mdi-home', link: "/" },
-        { title: 'Register Compliant', icon: 'mdi-note-plus', link : "/register-complaint" },
-        { title: 'Create Account', icon: 'mdi-login', link: "/create-account" },
-      ],
-       links: [
+    name:'App',
+    data (){
+      return{
+
+      }
+    }
+       ,links: [
         'Home',
         'About Us',
         'Team',
@@ -89,6 +49,6 @@
         'Contact Us',
       ],
 
-     }),
-  }
+     };
+  
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -21,10 +21,7 @@ const routes = [
     path: '/create-account',
     name: 'create-account',
     component: CreateAccount,
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    
   }
 ]
 
