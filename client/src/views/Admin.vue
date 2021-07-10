f582a4fbfe5af226cc6d226d7aa29236bfe591a9
Meron Abate <meriab1234@gmail.com>
2021-07-10 12:29:16 +0300

    with admin

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -1,9 +1,43 @@
 <template>
-<nav> Hello</nav>
+<nav> 
+    <v-toolbar color="blue-grey darken-4" >
+      
+       <v-icon color="white" @click="drawer = !drawer">view_sidebar</v-icon>
+        </v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light white--text">Welcome  </span>
+        <span class="white--text"> Admin</span>
+
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-btn flat color="blue-grey darken-4">
+           
+            <v-icon right color="white">circle_notifications</v-icon>
+        </v-btn>
+    
+        <v-btn flat color="blue-grey darken-4">
+            <span class="white--text" >Sign Out</span>
+            <v-icon right color="white">exit_to_app</v-icon>
+        </v-btn>
+    </v-toolbar>
+
+<v-navigation-drawer v-model="drawer" app class="blue-grey lighten-5">
+    <p class="display-2">WCSCMS</p>
+</v-navigation-drawer>
+
+
+
+
+
+</nav>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            drawer: false
+        }
+    }
 }
 </script>
--- client/src/views/CreateAccount.vue
+++ client/src/views/CreateAccount.vue
@@ -1,7 +1,33 @@
 <template>
-    <h1> Create user account</h1>
+<v-card>
+   <v-card-title>
+       <h2>Create a Customer account</h2>
+   </v-card-title>
+
+<v-card-text>
+    <v-form>
+    <v-text-field lable ="name" v-model="title">
+    </v-text-field>
+    </v-form>
+</v-card-text>
+
+</v-card>
+ </template>
+     
+  
+
+
+<script>
+export default {
+    data() {
+        return {
+            title: ''
+        }
+        
+    }
     
-</template>
+}
+</script>
 
 
 
